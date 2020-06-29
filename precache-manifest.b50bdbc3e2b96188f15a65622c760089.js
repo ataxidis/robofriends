1e1c688b77b31d3423c93ae0061695c541176099
Tasos <an.taxides@gmail.com>
2020-06-29 15:49:32 +0300

    Updates

---
+++ precache-manifest.b50bdbc3e2b96188f15a65622c760089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed4022d710113af525592cbe688c7dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "25fde944f9c499bf97d0",
+    "url": "/static/css/main.9b7e917f.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fde944f9c499bf97d0",
+    "url": "/static/js/main.bb38aa72.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  }
+]);
